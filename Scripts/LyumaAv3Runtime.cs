f2dddd921b56fa1d6478f16768b5ca56808385c1
Lyuma <xn.lyuma@gmail.com>
2022-02-16 11:57:45 -0800

    Convert all files to unix line endings; update copyright.

--- Scripts/LyumaAv3Runtime.cs
+++ Scripts/LyumaAv3Runtime.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright (c) 2020 Lyuma <xn.lyuma@gmail.com>
+/* Copyright (c) 2020-2022 Lyuma <xn.lyuma@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
