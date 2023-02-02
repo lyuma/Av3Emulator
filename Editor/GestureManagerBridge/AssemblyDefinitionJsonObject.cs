97942c141d02529dd0bde51b3b3f8c5892090788
jellejurre <jelle@jilles.com>
2023-02-02 13:46:06 +0100

    Add automatic Gesture manager detection and conversion

---
+++ Editor/GestureManagerBridge/AssemblyDefinitionJsonObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Codice.CM.Client.Differences;
+
+namespace Editor.GestureManagerBridge
+{
+	[Serializable]
+	public class AssemblyDefinitionJsonObject
+	{
+		public string name;
+		public string[] references;
+		public string[] includePlatforms;
+		public string[]	excludePlatforms;
+		public bool allowUnsafeCode;
+		public bool overrideReferences;
+		public string[]	precompiledReferences;
+		public bool autoReferenced;
+		public string[]	defineConstraints;
+		public string[]	versionDefines;
+		public bool noEngineReferences;
+	}
+}
---
+++ Editor/GestureManagerBridge/GestureManagerDetector.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BestHTTP.JSON;
+using Boo.Lang.Runtime;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Editor.GestureManagerBridge
+{
+	[InitializeOnLoad, ExecuteInEditMode]
+	public class GestureManagerDetector
+	{
+		static GestureManagerDetector()
+		{
+			DetectGestureManager();
+		}
+
+		public static void DetectGestureManager()
+		{
+			Type gestureManagerType = GetTypeFromName("BlackStartX.GestureManager.GestureManager");
+			bool definesChanged = false;
+			string existingDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+			HashSet<string> defineSet = new HashSet<string>();
+
+			if(existingDefines.Length > 0)
+				defineSet = new HashSet<string>(existingDefines.Split(';'));
+
+			string[] dependencies = new[]
+			{
+				"vrchat.blackstartx.gesture-manager",
+				"vrchat.blackstartx.gesture-manager.editor"
+			};
+			
+			if (gestureManagerType == null)
+			{
+				if(defineSet.Remove("GESTURE_MANAGER")) 
+					definesChanged = true;
+				RemoveFromAssembly(dependencies);
+			}
+			else
+			{
+				if(defineSet.Add("GESTURE_MANAGER")) 
+					definesChanged = true;
+				AddToAssembly(dependencies);
+			}
+			
+			if(definesChanged)
+			{
+				string finalDefineString = string.Join(";", defineSet.ToArray());
+				PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, finalDefineString);
+				Debug.LogFormat("Set Scripting Define Symbols for selected build target ({0}) to: {1}", EditorUserBuildSettings.selectedBuildTargetGroup.ToString(), finalDefineString);
+			}
+		}
+
+		public static void AddToAssembly(string[] references)
+		{
+			var files = Directory.GetFiles(Path.GetDirectoryName(Application.dataPath),
+				"lyuma.av3emulator.Editor.asmdef", SearchOption.AllDirectories);
+			if (files.Length == 0)
+			{
+				throw new RuntimeException("Can't find reference");
+			}
+			string text = File.ReadAllText(files[0]);
+			AssemblyDefinitionJsonObject def = JsonUtility.FromJson<AssemblyDefinitionJsonObject>(text);
+			string[] assemblyReferences = def.references.Concat(references).Distinct().ToArray();
+			def.references = assemblyReferences;
+
+			File.WriteAllText(files[0], JsonUtility.ToJson(def));
+		}
+		
+		public static void RemoveFromAssembly(string[] references)
+		{
+			var files = Directory.GetFiles(Path.GetDirectoryName(Application.dataPath),
+				"lyuma.av3emulator.Editor.asmdef", SearchOption.AllDirectories);
+			if (files.Length == 0)
+			{
+				throw new RuntimeException("Can't find reference");
+			}
+
+			string text = File.ReadAllText(files[0]);
+			AssemblyDefinitionJsonObject def = JsonUtility.FromJson<AssemblyDefinitionJsonObject>(text);
+			List<String> assemblyReferences = def.references.ToList();
+			foreach (var reference in references)
+			{
+				assemblyReferences.Remove(reference);
+			}
+
+			def.references = assemblyReferences.ToArray();
+			File.WriteAllText(files[0], JsonUtility.ToJson(def));
+		}
+
+		public static Type GetTypeFromName(string typeName)
+		{
+			var type = Type.GetType(typeName);
+			if (type != null)
+				return type;
+			foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
+			{
+				type = a.GetType(typeName);
+				if (type != null)
+					return type;
+			}
+
+			return null;
+		}
+	}
+}
