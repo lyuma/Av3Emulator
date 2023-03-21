70dee5b4df24acc9c0e858135305955705af7124
Dreadrith <valorinferno@gmail.com>
2023-03-21 11:26:45 +0300

    Implement descriptor colliders & senders emulation

--- Runtime/Scripts/LyumaAv3Emulator.cs
+++ Runtime/Scripts/LyumaAv3Emulator.cs
@@ -19,7 +19,13 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 using VRC.SDK3.Avatars.Components;
+using System.Reflection;
+using UnityEditor;
+using VRC.Dynamics;
+using VRC.SDK3.Dynamics.Contact.Components;
+using VRC.SDK3.Dynamics.PhysBone.Components;
 
 namespace Lyuma.Av3Emulator.Runtime
 {
@@ -33,7 +39,8 @@ namespace Lyuma.Av3Emulator.Runtime
 		public bool DefaultTestInStation = false;
 		public LyumaAv3Runtime.TrackingTypeIndex DefaultTrackingType = LyumaAv3Runtime.TrackingTypeIndex.HeadHands;
 		[Header("Emulation")]
-		public VRCAvatarDescriptor.AnimLayerType DefaultAnimatorToDebug = VRCAvatarDescriptor.AnimLayerType.Base;
+		public VRCAvatarDescriptor.AnimLayerType DefaultAnimatorToDebug = VRCAvatarDescriptor.AnimLayerType.Base; 
+        public DescriptorExtractionType DescriptorColliders = DescriptorExtractionType.CollidersAndSenders;
 		public bool RestartEmulator;
 		private bool RestartingEmulator;
 		[Tooltip("Simulate behavior with sub-animator parameter drivers prior to the 2021.1.1 patch (19 Jan 2021)")]
@@ -108,7 +115,13 @@ namespace Lyuma.Av3Emulator.Runtime
 						runtime.CreateMirrorClone();
 					}
 					runtime.DisableMirrorAndShadowClones = DisableShadowClone && DisableMirrorClone;
-				} catch (System.Exception e) {
+                    if (DescriptorColliders != DescriptorExtractionType.None)
+                    {
+                        ForceUpdateDescriptorColliders(avadesc);
+                        ExtractDescriptorColliders(avadesc, DescriptorColliders);
+                    }
+
+                } catch (System.Exception e) {
 					Debug.LogException(e);
 				}
 			}
@@ -172,5 +185,139 @@ namespace Lyuma.Av3Emulator.Runtime
 			}
 		}
 
-	}
+        private void ForceUpdateDescriptorColliders(VRCAvatarDescriptor descriptor)
+        {
+            Editor tempEditor = null;
+            try
+            {
+				
+                var descriptorEditor = System.Type.GetType("AvatarDescriptorEditor3, Assembly-CSharp-Editor, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+                if (descriptorEditor == null) descriptorEditor = System.Type.GetType("AvatarDescriptorEditor3, VRC.SDK3A.Editor, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+                var editorAvatarField = descriptorEditor.GetField("avatarDescriptor", BindingFlags.NonPublic | BindingFlags.Instance);
+                var updateMethod = descriptorEditor.GetMethod("UpdateAutoColliders", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                tempEditor = Editor.CreateEditor(descriptor, descriptorEditor);
+                editorAvatarField.SetValue(tempEditor, descriptor);
+                updateMethod.Invoke(tempEditor, null);
+                DestroyImmediate(tempEditor);
+            }
+            catch
+            {
+                Debug.LogError("Failed to force update Descriptor Colliders through reflection.");
+                if (tempEditor != null) DestroyImmediate(tempEditor);
+            }
+
+        }
+
+        public enum DescriptorExtractionType
+        {
+            None = 0,
+            Colliders = 1 << 0,
+			Senders = 1 << 1,
+            CollidersAndSenders = ~0,
+        }
+        private void ExtractDescriptorColliders(VRCAvatarDescriptor descriptor, DescriptorExtractionType extractionType)
+        {
+			var animator = descriptor.GetComponent<Animator>();
+            if (!animator)
+            {
+                Debug.LogError($"No animator found on {descriptor.gameObject.name}; Can't extract descriptor colliders.");
+				return;
+            }
+
+            List<VRCPhysBoneCollider> createdColliders = new List<VRCPhysBoneCollider>();
+            var collidingBones = descriptor.GetComponentsInChildren<VRCPhysBone>().Where(p => p.allowCollision);
+
+            void ExtractCollider(VRCAvatarDescriptor.ColliderConfig config, HumanBodyBones matchedBone, string collisionName, bool isSenderOnly = false)
+            {
+                if (config.state != VRCAvatarDescriptor.ColliderConfig.State.Disabled)
+                {
+                    var target = config.transform;
+
+                    if (target)
+                    {
+						//For some reason the visuals followed the parent of finger bones but has the local position of the finger as offset
+						//Haven't yet confirmed if this is translated in-game but it should be
+						Vector3 localOffset = Vector3.zero;
+                        if (IsFingerBone(matchedBone))
+                        {
+							//Sometimes users switch the transform to be something that isn't a finger
+							//Would have to go through the animator's hierarchy and double check whether this is still a non proximal finger bone
+							localOffset = target.transform.localPosition;
+                            target = target.parent;
+                        }
+
+                        var collider = target.gameObject.AddComponent<VRCPhysBoneCollider>();
+                        var sender = target.gameObject.AddComponent<VRCContactSender>();
+
+                        collider.radius = sender.radius = config.radius;
+                        collider.height = sender.height = config.height;
+                        collider.position = sender.position = config.position + localOffset;
+                        collider.rotation = sender.rotation = config.rotation;
+                        collider.rootTransform = sender.rootTransform = target;
+
+                        collider.shapeType = VRCPhysBoneColliderBase.ShapeType.Capsule;
+                        sender.shapeType = ContactBase.ShapeType.Capsule;
+
+                        sender.collisionTags.Add(collisionName);
+                        if (HasMirrorBone(matchedBone))
+                            sender.collisionTags.Add(collisionName + (IsLeftBone(matchedBone) ? "L" : "R"));
+
+                        if (isSenderOnly || !extractionType.HasFlag(DescriptorExtractionType.Colliders)) DestroyImmediate(collider);
+                        else createdColliders.Add(collider);
+
+						if (!extractionType.HasFlag(DescriptorExtractionType.Senders)) 
+                            DestroyImmediate(sender);
+                    }
+                }
+            }
+
+            ExtractCollider(descriptor.collider_head, HumanBodyBones.Head, "Head", true);
+            ExtractCollider(descriptor.collider_torso, HumanBodyBones.Chest, "Torso", true);
+            ExtractCollider(descriptor.collider_footR, HumanBodyBones.RightToes, "Foot", true);
+            ExtractCollider(descriptor.collider_footL, HumanBodyBones.LeftToes, "Foot", true);
+            ExtractCollider(descriptor.collider_handR, HumanBodyBones.RightHand, "Hand");
+            ExtractCollider(descriptor.collider_fingerLittleR, HumanBodyBones.RightLittleIntermediate, "FingerLittle");
+            ExtractCollider(descriptor.collider_fingerIndexR, HumanBodyBones.RightIndexIntermediate, "FingerIndex");
+            ExtractCollider(descriptor.collider_fingerMiddleR, HumanBodyBones.RightMiddleIntermediate, "FingerMiddle");
+            ExtractCollider(descriptor.collider_fingerRingR, HumanBodyBones.RightRingIntermediate, "FingerRing");
+            ExtractCollider(descriptor.collider_handL, HumanBodyBones.LeftHand, "Hand");
+            ExtractCollider(descriptor.collider_fingerLittleL, HumanBodyBones.LeftLittleIntermediate, "FingerLittle");
+            ExtractCollider(descriptor.collider_fingerIndexL, HumanBodyBones.LeftIndexIntermediate, "FingerIndex");
+            ExtractCollider(descriptor.collider_fingerMiddleL, HumanBodyBones.LeftMiddleIntermediate, "FingerMiddle");
+            ExtractCollider(descriptor.collider_fingerRingL, HumanBodyBones.LeftRingIntermediate, "FingerRing");
+
+            if (extractionType.HasFlag( DescriptorExtractionType.Colliders))
+                foreach (var p in collidingBones)
+                    p.colliders.AddRange(createdColliders);
+        }
+
+        internal static Transform SmartGetBone(Animator animator, HumanBodyBones humanBone)
+        {
+            int boneIndex = (int)humanBone;
+            Transform b = animator.GetBoneTransform((HumanBodyBones)boneIndex);
+            if (b) return b;
+
+            if (IsFingerBone(humanBone) && boneIndex%3 != 0)
+                b = SmartGetBone(animator, (HumanBodyBones)(--boneIndex));
+            else if (IsToeBone(humanBone))
+            {
+                boneIndex -= 14;
+                b = animator.GetBoneTransform((HumanBodyBones)boneIndex);
+            }
+
+            return b;
+        }
+
+        internal static bool IsFingerBone(HumanBodyBones bone) => (int)bone >= 24 && (int)bone <= 53;
+        internal static bool IsToeBone(HumanBodyBones bone) => (int) bone == 19 || (int) bone == 20;
+        internal static bool IsLeftBone(HumanBodyBones bone) => bone.ToString().Contains("Left");
+
+        internal static bool HasMirrorBone(HumanBodyBones bone)
+        {
+            string name = bone.ToString();
+            return name.Contains("Left") || name.Contains("Right");
+        }
+
+    }
 }
