124567547b12ba761039156fdf3ba7110d550f9c
jellejurre <jelle@jilles.com>
2023-09-12 11:24:40 +0200

    Actually disable objects and enable them once we're initializing

--- Runtime/Scripts/LyumaAv3Emulator.cs
+++ Runtime/Scripts/LyumaAv3Emulator.cs
@@ -220,6 +220,7 @@ namespace Lyuma.Av3Emulator.Runtime
 			Debug.Log(this.name + ": Setting up Av3Emulator on " + avatars.Count + " avatars.", this);
 			foreach (var avadesc in avatars)
 			{
+				avadesc.SetActive(true);
 				if (avadesc.GetComponents<Component>().Any(x => x.GetType().Name == "PipelineSaver")) {
 					Debug.Log("Found PipelineSaver on " + avadesc.name + ". Disabling clones and mirror copy.", avadesc);
 					DisableMirrorClone = true;
